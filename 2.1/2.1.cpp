0e60be43a61fbdab95b6ada72c218f84a42e9ee2
Samuele Torresani <samuele.torresani01@gmail.com>
2020-11-02 15:36:48 +0100

    initial commit

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,24 @@
+/*
+    SUP che legge 10 numeri dall'utente e ne calcola la somma. Fermarsi a leggere numeri quando l'utente
+    inserisce tutti i 10 numeri oppure quando inserisce 0, stabilendo un numero massimo a priori.
+    Info: Il limite massimo dipende da quanto grandi decidiamo che siano le strutture in fase di compilazione. Impareremo che è possibile scrivere programmi che non hanno questo tipo di limite.
+*/
+
+#include <iostream>
+using namespace std;
+
+#define N 10
+
+int main(){
+    int number[N];
+
+    cout<< "insert " <<N<< "number "<<endl;
+    for ( int i=0; i< N; i++){
+        cin>> number [i];
+    }
+    int sum =0;
+    for ( int i=0; i< N; i++){
+        sum += number [i];
+    }
+cout<<"sum= "<<sum;
+}
